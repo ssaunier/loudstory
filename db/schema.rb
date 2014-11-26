c71ab3a2f6d0201109defff9f3f08a2d6f239719
Fleur Broca <brocafleur@gmail.com>
2014-11-26 18:46:28 +0100

    add model session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125180518) do
+ActiveRecord::Schema.define(version: 20141126173459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,17 @@ ActiveRecord::Schema.define(version: 20141125180518) do
   end
 
   add_index "questions", ["exercise_id"], name: "index_questions_on_exercise_id", using: :btree
+
+  create_table "sessions", force: true do |t|
+    t.string   "name"
+    t.integer  "track_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["track_id"], name: "index_sessions_on_track_id", using: :btree
+  add_index "sessions", ["user_id"], name: "index_sessions_on_user_id", using: :btree
 
   create_table "tracks", force: true do |t|
     t.string   "title"
